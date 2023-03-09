8d577956aeef45de3435e1507a658b0f7a8054d6
laisrmacedo <laisrodriguesmacedo@gmail.com>
2023-03-09 10:32:59 +0100

    feat: POST endpoint for route registration

---
+++ src/router/formRouter.ts
@@ -0,0 +1,16 @@
+import express from "express"
+import { FormBusiness } from "../business/FormBusiness"
+import { FormController } from "../controller/FormController"
+import { FormDatabase } from "../database/FormDatabase"
+import { FormDTO } from "../dto/FormDTO"
+
+export const formRouter = express.Router()
+
+const formController = new FormController(
+  new FormDTO(),
+  new FormBusiness(
+    new FormDatabase()
+  )
+)
+
+formRouter.post("/", formController.registration)
