8ae6f2e0a6ee35ee7ba4344f1544851714d2583a
laisrmacedo <laisrodriguesmacedo@gmail.com>
2023-03-09 11:56:35 +0100

    feat: public method getFormsBetweenDates of the controller layer

--- src/controller/FormController.ts
+++ src/controller/FormController.ts
@@ -30,4 +30,24 @@ export class FormController {
       }
     }
   }
+
+  public getFormsBetweenDates = async (req: Request, res: Response): Promise<void> => {
+    try {
+      const input = this.formDTO.getFormsBetweenDatesInputDTO(
+        req.params.initial,
+        req.params.final
+      )
+
+      const output = await this.formBusiness.getFormsBetweenDates(input)
+      res.status(200).send(output)
+
+    } catch (error) {
+      console.log(error)
+      if (error instanceof BaseError) {
+        res.status(error.statusCode).send(error.message)
+      } else {
+        res.status(500).send("Unexpected error")
+      }
+    }
+  }
 }
