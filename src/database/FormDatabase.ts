62d38bef8c4786dacbaa2969ca356eed9eaacc1d
laisrmacedo <laisrodriguesmacedo@gmail.com>
2023-03-09 20:46:31 +0100

    refactor: environment variable to port of PostGresSQL server

--- src/database/FormDatabase.ts
+++ src/database/FormDatabase.ts
@@ -7,7 +7,7 @@ const knex = require('knex')({
   version: '7.2',
   connection: {
     host : '127.0.0.1',
-    port : 5432,
+    port : process.env.DB_PORT,
     user : process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database : 'forms_answers'
