2bd56492afab0d9602a0c40f6d60b80960b2993c
laisrmacedo <laisrodriguesmacedo@gmail.com>
2023-03-09 10:38:51 +0100

    feat: public method registration of the controller layer

---
+++ src/controller/FormController.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from "express";
+import { FormBusiness } from "../business/FormBusiness";
+import { FormDTO } from "../dto/FormDTO";
+import { BaseError } from "../errors/BaseError";
+
+export class FormController {
+  constructor(
+    private formDTO: FormDTO,
+    private formBusiness: FormBusiness
+  ){}
+
+  public registration = async (req: Request, res: Response): Promise<void> => {
+    try {
+      const input = this.formDTO.registrationInputDTO(
+        req.body.name,
+        req.body.email,
+        req.body.cpf,
+        req.body.phone
+      )
+
+      const output = await this.formBusiness.registration(input)
+      res.status(201).send(output)
+
+    } catch (error) {
+      console.log(error)
+      if (error instanceof BaseError) {
+        res.status(error.statusCode).send(error.message)
+      } else {
+        res.status(500).send("Unexpected error")
+      }
+    }
+  }
+}
