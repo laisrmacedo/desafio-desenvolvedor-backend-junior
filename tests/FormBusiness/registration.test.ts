b7a61170ef5e6b432403a9dc3a0d2fcfa5e4650d
laisrmacedo <laisrodriguesmacedo@gmail.com>
2023-03-09 15:49:26 +0100

    test: email format

--- tests/FormBusiness/registration.test.ts
+++ tests/FormBusiness/registration.test.ts
@@ -19,5 +19,22 @@ describe("registration", () => {
     await formBusiness.registration(input)
   })
 
-  
+  it("test email format", async () => {
+    expect.assertions(2)
+    try {
+      const input = {
+        name: 'clientTest',
+        email: 'incorrectFormat',
+        cpf: '22222222222',
+        phone: '(22)99999-9999'
+      }
+
+      await formBusiness.registration(input)
+    } catch (error) {
+      if (error instanceof BadRequestError) {
+        expect(error.message).toBe("ERROR: 'email' must be like 'example@example.example'.")
+        expect(error.statusCode).toBe(400)
+      }
+    }
+  })
 })
